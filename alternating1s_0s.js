dfa908e1704fcb8f69dfa23fde27c6abaefdb882
Keenah <kvancamp@mail.com>
2023-02-15 18:03:38 -0600

    Create alternating1s_0s.js

---
+++ alternating1s_0s.js
@@ -0,0 +1,13 @@
+/* Write a function that takes one argument, a positive integer, and retruns a string of alternating '1's and '0's, always starting with a '1'. The length of the string should match the given integer */
+
+/* 
+INPUT:
+  - one positive integer 
+OUTPUT:
+ - string of alternating 1s and 0s
+RULES:
+ - string starts with a '1'
+ - length should match input amount
+ - return empty string if unput is - or less than 0
+ 
+*/
