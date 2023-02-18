eb056da0b5299da662a025962a8d072ee86a11dd
Keenah <kvancamp@mail.com>
2023-02-17 19:20:59 -0600

    Update sum_mixed_array.js

--- sum_mixed_array.js
+++ sum_mixed_array.js
@@ -4,3 +4,8 @@ Given an array of integers as strings and numbers, return the sum of the array v
 Return your answer as a number. */
 
 sumMix = (x) => x.reduce((prev, current) => +current + prev, 0);
+
+//or
+function sumMix(x) {
+  return x.map((a) => +a).reduce((a, b) => a + b);
+}
