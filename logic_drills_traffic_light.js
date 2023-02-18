7e0e97f0defb6ad12aa135ace075bbb7798bd1d6
Keenah <kvancamp@mail.com>
2023-02-17 19:20:56 -0600

    Create logic_drills_traffic_light.js

---
+++ logic_drills_traffic_light.js
@@ -0,0 +1,16 @@
+/* You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+
+
+changes: green, yellow, red, green
+
+string as arguement: 
+string for future color
+
+*/
+
+const updateLight = (current) =>
+  current == "red" ? "green" : current == "green" ? "yellow" : "red";
