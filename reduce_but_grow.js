67d378caeade7e4e8d9f56b93370ed171bce8ccb
Keenah <kvancamp@mail.com>
2023-02-18 16:28:48 -0600

    Update reduce_but_grow.js

--- reduce_but_grow.js
+++ reduce_but_grow.js
@@ -8,10 +8,4 @@ return integer
 
 */
 
-function grow(x) {
-  var sum = 1;
-  for (var i = 0; i < x.length; i++) {
-    sum = sum * x[i];
-  }
-  return sum;
-}
+const grow = (x) => x.reduce((a, b) => a * b);
